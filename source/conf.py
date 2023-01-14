182ed69b7f690e6c310eed78c191e0a95d2026cb
ftnext <takuyafjp+develop@gmail.com>
2023-01-14 17:14:42 +0900

    [chore] Add revealjs configuration
    from 2022_slides repo

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,9 @@ author = 'nikkie'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    "sphinx_revealjs",
     'sphinx.ext.githubpages',
+    "sphinx_design",
 ]
 
 templates_path = ['_templates']
@@ -27,3 +29,34 @@ language = 'ja'
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# -- Options for Reveal.js output -------------------------------------------------
+
+revealjs_static_path = ["_static"]
+revealjs_script_conf = """
+    {
+        controls: true,
+        progress: true,
+        history: true,
+        center: true,
+        transition: "none",
+        slideNumber: "c/t",
+    }
+"""
+
+revealjs_script_plugins = [
+    {
+        "name": "RevealHighlight",
+        "src": "revealjs4/plugin/highlight/highlight.js",
+    },
+    {
+        "name": "RevealNotes",
+        "src": "revealjs4/plugin/notes/notes.js",
+    }
+]
+
+revealjs_css_files = [
+    "revealjs4/plugin/highlight/zenburn.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css",
+    "css/common.css",
+]
