17350873917e450c5730cbb32964bea060751aef
ftnext <takuyafjp+develop@gmail.com>
2023-01-20 19:11:16 +0900

    [feat] Implement 'add date' CodeLens

--- samplecode/awakening-extension/markdown-date/src/extension.ts
+++ samplecode/awakening-extension/markdown-date/src/extension.ts
@@ -1,16 +1,74 @@
+import dayjs = require("dayjs");
 import * as vscode from "vscode";
+
+let disposables: vscode.Disposable[] = [];
+const titleRegex = /^#+\s/g;
+
+export class CodelensProvider implements vscode.CodeLensProvider {
+  constructor() {}
+
+  public provideCodeLenses(
+    document: vscode.TextDocument,
+    token: vscode.CancellationToken
+  ): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
+    const codeLenses = [];
+    const regex = new RegExp(titleRegex);
+    const text = document.getText();
+    let matches;
+    while ((matches = regex.exec(text)) !== null) {
+      const line = document.lineAt(document.positionAt(matches.index).line);
+      const indexOf = line.text.indexOf(matches[0]);
+      const position = new vscode.Position(line.lineNumber, indexOf);
+      const range = document.getWordRangeAtPosition(
+        position,
+        new RegExp(titleRegex)
+      );
+      if (range) {
+        // [コマンドを呼び出すようにCodeLensを作成]
+        codeLenses.push(
+          new vscode.CodeLens(range, {
+            title: "add date",
+            tooltip: "add date",
+            command: "markdown-date.addDate",
+            arguments: [range],
+          })
+        );
+        // [コマンドを呼び出すようにCodeLensを作成]
+      }
+    }
+    return codeLenses;
+  }
+
+  public resolveCodeLens(
+    codeLens: vscode.CodeLens,
+    token: vscode.CancellationToken
+  ) {
+    return codeLens;
+  }
+}
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "markdown-date" is now active!');
 
-  let disposable = vscode.commands.registerCommand(
-    "markdown-date.helloWorld",
-    () => {
-      vscode.window.showInformationMessage("Hello World from Markdown Date!");
+  const codelensProvider = new CodelensProvider();
+  let disposable = vscode.languages.registerCodeLensProvider(
+    { language: "markdown" },
+    codelensProvider
+  );
+  disposables.push(disposable);
+
+  vscode.commands.registerCommand(
+    "markdown-date.addDate",
+    (range: vscode.Range) => {
+      if (vscode.window.activeTextEditor) {
+        const text = vscode.window.activeTextEditor.document.getText(range);
+        const today = dayjs().format("YYYY-MM-DD");
+        vscode.window.activeTextEditor.edit((editBuilder) => {
+          editBuilder.replace(range, text + today + " ");
+        });
+      }
     }
   );
-
-  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
