73d61dd621af153fdea1c367f79c594ae91d97cc
ftnext <takuyafjp+develop@gmail.com>
2023-01-20 18:56:29 +0900

    [style] Format

--- samplecode/awakening-extension/markdown-date/src/extension.ts
+++ samplecode/awakening-extension/markdown-date/src/extension.ts
@@ -1,13 +1,16 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "markdown-date" is now active!');
+  console.log('Congratulations, your extension "markdown-date" is now active!');
 
-	let disposable = vscode.commands.registerCommand('markdown-date.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from Markdown Date!');
-	});
+  let disposable = vscode.commands.registerCommand(
+    "markdown-date.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello World from Markdown Date!");
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
