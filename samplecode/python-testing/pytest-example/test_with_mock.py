b69c83c73453f546d9945da90206acef23672aa8
ftnext <takuyafjp+develop@gmail.com>
2023-10-26 23:30:11 +0900

    [bugfix] Add missing parameters

--- samplecode/python-testing/pytest-example/test_with_mock.py
+++ samplecode/python-testing/pytest-example/test_with_mock.py
@@ -1,16 +1,17 @@
+import random
 from unittest.mock import patch
 
 
-def a_func():
-    ...
+def a_func(number: int):
+    print(number)
 
 
-def b_func():
-    ...
+def b_func(foo: str, bar: str):
+    print(foo, bar)
 
 
 def c_func():
-    ...
+    print(random.randint(1, 6))
 
 
 def foo():
@@ -28,3 +29,7 @@ def test_foo(a_func, b_func, c_func):
     a_func.assert_called_once_with(42)
     b_func.assert_called_once_with("ham", "egg")
     c_func.assert_called_once_with()
+
+
+if __name__ == "__main__":
+    foo()
